791ec32afb1b3d9f2cc3551f9db18bd4b7b3dbe7
Muhammad Rehan Baig <mrb5only@gmail.com>
2020-03-26 23:54:27 +0500

    adding generic sql Ado.net

---
+++ SGA/Repository/Helpers/Implementations/Dbhelper.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using Repository.Helpers.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Helpers.Implementations
+{
+    public class Dbhelper : IDBhelper
+    {
+        private readonly IConfiguration _configuration;
+        private readonly SqlConnection _connection;
+        private readonly string _connectionString;
+        public Dbhelper(IConfiguration config)
+        {
+            _configuration = config;
+            _connectionString = _configuration.GetConnectionString("Db");
+            _connection = new SqlConnection(_connectionString);
+        }
+
+        public void Dispose()
+        {
+            _connection.Close();
+        }
+
+        public bool ExecuteNonQuery(string sp, DbParameter[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ExecuteNonQuery(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataTable> Get(string sp, DbParameter[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataTable> Get(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(string tblName, DataTable data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(string sp, DbParameter[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SGA/Repository/Helpers/Interfaces/IDBhelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Helpers.Interfaces
+{
+    public interface IDBhelper
+    {
+        Task<DataTable> Get(string sp, DbParameter[] parameters);
+        Task<DataTable> Get(string query);
+        bool Insert(string tblName, DataTable data);
+        bool Insert(string sp, DbParameter[] parameters);
+        bool ExecuteNonQuery(string sp, DbParameter[] parameters);
+        bool ExecuteNonQuery(string query);
+        void Dispose();
+
+    }
+}
