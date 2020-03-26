b7a9ab7b2cbd54a965960e90a59252f4e0791b00
Muhammad Rehan Baig <mrb5only@gmail.com>
2020-03-26 15:54:36 +0500

    Entityframework removed from project

---
+++ SGA/SGA/Data/SGAContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Model.Entities;
+
+namespace SGA.Models
+{
+    public class SGAContext : DbContext
+    {
+        public SGAContext (DbContextOptions<SGAContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Model.Entities.Shop> Shop { get; set; }
+    }
+}
